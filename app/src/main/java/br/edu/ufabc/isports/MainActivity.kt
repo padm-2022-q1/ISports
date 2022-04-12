052b8deccaa6db676374ca8e2655533794bef0d9
mateus.flosi <mateusflosimolero@hotmail.com>
2022-04-12 12:02:52 -0300

    Adição de xml de navegação e uso de binding

--- app/src/main/java/br/edu/ufabc/isports/MainActivity.kt
+++ app/src/main/java/br/edu/ufabc/isports/MainActivity.kt
@@ -2,10 +2,14 @@ package br.edu.ufabc.isports
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ufabc.isports.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
