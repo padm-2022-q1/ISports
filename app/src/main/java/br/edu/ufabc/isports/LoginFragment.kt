934726aebec55f9048dbdc2b61d77178ec6114da
mateus.flosi <mateusflosimolero@hotmail.com>
2022-04-23 00:55:27 -0300

    Autenticando usuário

--- app/src/main/java/br/edu/ufabc/isports/LoginFragment.kt
+++ app/src/main/java/br/edu/ufabc/isports/LoginFragment.kt
@@ -1,5 +1,6 @@
 package br.edu.ufabc.isports
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
 import br.edu.ufabc.isports.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment(){
     private lateinit var  binding: FragmentLoginBinding
@@ -24,17 +27,45 @@ class LoginFragment : Fragment(){
     override fun onStart() {
         super.onStart()
         activity?.let {
+            if(FirebaseAuth.getInstance().currentUser != null){
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMeusJogosFragment(), navOptions {
+                    popUpTo(findNavController().graph.startDestinationId){
+                        inclusive=true
+                    }
+                })
+            }
             bindEvents()
         }
     }
 
     private fun bindEvents() {
-        binding.loginButton.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMeusJogosFragment(), navOptions {
-                popUpTo(findNavController().graph.startDestinationId){
-                    inclusive=true
-                }
-            })
+        binding.loginButton.setOnClickListener { view ->
+            val email = binding.loginEmailEditText.text.toString()
+            val password = binding.loginPasswordEditText.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()){
+                Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_SHORT)
+                    .setBackgroundTint(Color.GRAY)
+                    .setTextColor(Color.BLACK)
+                    .show()
+            } else{
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMeusJogosFragment(), navOptions {
+                                popUpTo(findNavController().graph.startDestinationId){
+                                    inclusive=true
+                                }
+                            })
+                        } else{
+                            Snackbar.make(view, "Erro no login", Snackbar.LENGTH_SHORT)
+                                .setBackgroundTint(Color.GRAY)
+                                .setTextColor(Color.BLACK)
+                                .show()
+                        }
+                    }
+            }
         }
         binding.cadastroTextView.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_cadastroFragment)
