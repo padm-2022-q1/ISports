7ad08bdd269190ca5b49475d087a3a1ab6ec1284
mateus.flosi <mateusflosimolero@hotmail.com>
2022-04-23 01:11:37 -0300

    logout e pegando username para preecher na tela de perfil

--- app/src/main/java/br/edu/ufabc/isports/PerfilFragment.kt
+++ app/src/main/java/br/edu/ufabc/isports/PerfilFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import br.edu.ufabc.isports.databinding.FragmentPerfilBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class PerfilFragment : Fragment() {
     private lateinit var binding: FragmentPerfilBinding
@@ -17,9 +19,23 @@ class PerfilFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPerfilBinding.inflate(inflater, container, false)
-        binding.bottomNavigationPerfil.selectedItemId = R.id.menu_perfil
-        bindEvents()
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        activity?.let{
+            binding.bottomNavigationPerfil.selectedItemId = R.id.menu_perfil
+            FirebaseFirestore.getInstance().collection("Usuarios")
+                .document(FirebaseAuth.getInstance().currentUser!!.uid)
+                .addSnapshotListener { value, _ ->
+                    if(value != null){
+                        binding.perfilUsername.text = value.getString("username")
+                    }
+                }
+
+            bindEvents()
+        }
     }
 
     private fun bindEvents() {
@@ -34,7 +50,8 @@ class PerfilFragment : Fragment() {
         binding.alterarCadastroButton.setOnClickListener {
             findNavController().navigate(PerfilFragmentDirections.actionPerfilFragmentToCadastroFragment(isAlteracao = true))
         }
-        binding.logoutButton.setOnClickListener{
+        binding.logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
             findNavController().navigate(R.id.action_perfilFragment_to_loginFragment)
         }
     }
