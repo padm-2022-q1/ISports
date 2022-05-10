757e8c1430bcda2549d497d15279a4eba7eab455
mateus.flosi <mateusflosimolero@hotmail.com>
2022-05-09 21:09:30 -0300

    Apagando referencias que não são mais utilizadas

--- app/src/main/java/br/edu/ufabc/isports/MainViewModel.kt
+++ app/src/main/java/br/edu/ufabc/isports/MainViewModel.kt
@@ -6,15 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import br.edu.ufabc.isports.model.JogoFirestore
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository = (application as App).repository
-
     val clickedItemId by lazy {
         MutableLiveData<JogoFirestore>()
     }
-
-    fun allContacts() = repository.getAll()
-
-    fun getAt(position: Int) = repository.getAt(position)
-
-    fun getById(id: Long) = repository.getById(id)
 }
