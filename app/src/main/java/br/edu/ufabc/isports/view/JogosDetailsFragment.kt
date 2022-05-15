8b809d83d2a3cf735a0e005a1a9fa15245c13335
Kaio1505 <kaio1505@gmail.com>
2022-05-15 19:42:02 -0300

    Adicionado recycler view meus jogos e historico

--- app/src/main/java/br/edu/ufabc/isports/view/JogosDetailsFragment.kt
+++ app/src/main/java/br/edu/ufabc/isports/view/JogosDetailsFragment.kt
@@ -2,13 +2,21 @@ package br.edu.ufabc.isports.view
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.ufabc.isports.R
 import br.edu.ufabc.isports.databinding.FragmentJogosDetailsBinding
+import br.edu.ufabc.isports.databinding.ListParticipantesBinding
+import br.edu.ufabc.isports.model.Jogo
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
+import br.edu.ufabc.isports.model.Participantes
 import br.edu.ufabc.isports.viewModel.MainViewModel
 import com.google.android.material.snackbar.Snackbar
 
@@ -17,6 +25,72 @@ class JogosDetailsFragment : Fragment() {
     private lateinit var binding: FragmentJogosDetailsBinding
     private val args: JogosDetailsFragmentArgs by navArgs()
     private val viewModel: MainViewModel by activityViewModels()
+    companion object{
+        lateinit var participantes_username: List<String>
+    }
+
+    private inner class ContactAdapter(val contacts: List<String>) :
+        RecyclerView.Adapter<ContactAdapter.ContactHolder>() {
+
+
+        private inner class ContactHolder(itemBinding: ListParticipantesBinding) :
+            RecyclerView.ViewHolder(itemBinding.root) {
+
+            val participante = itemBinding.participante
+            }
+
+
+        /**
+         * Create a view holder.
+         */
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ContactHolder =
+            ContactHolder(
+                ListParticipantesBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        fun setIcon(icon:String):Int
+        {
+            return when (icon) {
+                "Basquete" -> {
+                    R.drawable.ic_baseline_sports_basketball
+                }
+                "Tenis" -> {
+                    R.drawable.ic_baseline_sports_tennis
+                }
+                "Vôlei" -> {
+                    R.drawable.ic_baseline_sports_volleyball
+                }
+                else -> R.drawable.ic_baseline_sports_soccer
+            }
+        }
+        /**
+         * Populate a view holder with data.
+         */
+        override fun onBindViewHolder(holder: ContactHolder, position: Int) {
+            val contact = contacts[position]
+
+            holder.participante.text = contact
+        }
+
+        /**
+         * The total quantity of items in the list.
+         */
+        override fun getItemCount(): Int = contacts.size
+
+        /**
+         * Called when a view holder is recycled.
+         */
+        override fun onViewRecycled(holder: ContactHolder) {
+            super.onViewRecycled(holder)
+            Log.d("APP", "Recycled holder at position ${holder.bindingAdapterPosition}")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,8 +122,14 @@ class JogosDetailsFragment : Fragment() {
     }
 
     private fun initTexts() {
+
         args.jogoItem.takeIf { it?.id?.isNotEmpty() == true }?.also { jogoItem ->
-            binding.jogoId.text = jogoItem.id
+            binding.modalidade.text = jogoItem.modalidade
+            binding.inicio.text = jogoItem.inicio.toString()
+            binding.fim.text = jogoItem.fim.toString()
+            binding.local.text = jogoItem.local
+            binding.recyclerviewParticipantes.apply {
+            }
         }
     }
 
--- app/src/main/java/br/edu/ufabc/isports/view/MeusJogosFragment.kt
+++ app/src/main/java/br/edu/ufabc/isports/view/MeusJogosFragment.kt
@@ -1,16 +1,100 @@
 package br.edu.ufabc.isports.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import androidx.recyclerview.widget.RecyclerView
 import br.edu.ufabc.isports.R
 import br.edu.ufabc.isports.databinding.FragmentMeusJogosBinding
+import br.edu.ufabc.isports.databinding.JogosListItemBinding
+import br.edu.ufabc.isports.model.Jogo
+import br.edu.ufabc.isports.viewModel.MainViewModel
 
 class MeusJogosFragment : Fragment() {
     private lateinit var binding: FragmentMeusJogosBinding
+    private val viewModel: MainViewModel by activityViewModels()
+
+    private inner class ContactAdapter(val contacts: List<Jogo>) :
+        RecyclerView.Adapter<ContactAdapter.ContactHolder>() {
+
+
+        private inner class ContactHolder(itemBinding: JogosListItemBinding) :
+            RecyclerView.ViewHolder(itemBinding.root) {
+
+            val modalidade = itemBinding.jogosListModalidade
+            val dateCreated = itemBinding.jogosListDataDia
+            val time = itemBinding.jogosListTime
+            val endereco = itemBinding.jogosListEndereco
+            val icon = itemBinding.notesListFavorites
+            init {
+                itemBinding.root.setOnClickListener {
+                    viewModel.clickedItemId.value = contacts[bindingAdapterPosition]
+                }
+
+            }
+        }
+
+        /**
+         * Create a view holder.
+         */
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ContactHolder =
+            ContactHolder(
+                JogosListItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        fun setIcon(icon:String):Int
+        {
+            return when (icon) {
+                "Basquete" -> {
+                    R.drawable.ic_baseline_sports_basketball
+                }
+                "Tenis" -> {
+                    R.drawable.ic_baseline_sports_tennis
+                }
+                "Vôlei" -> {
+                    R.drawable.ic_baseline_sports_volleyball
+                }
+                else -> R.drawable.ic_baseline_sports_soccer
+            }
+        }
+        /**
+         * Populate a view holder with data.
+         */
+        override fun onBindViewHolder(holder: ContactHolder, position: Int) {
+            val contact = contacts[position]
+
+            holder.modalidade.text = contact.modalidade
+            holder.dateCreated.text = contact.inicio.toString()
+            //holder.time.text = contact.timeStart
+            holder.endereco.text = contact.local
+            holder.icon.setImageResource(setIcon(contact.modalidade))
+        }
+
+        /**
+         * The total quantity of items in the list.
+         */
+        override fun getItemCount(): Int = contacts.size
+
+        /**
+         * Called when a view holder is recycled.
+         */
+        override fun onViewRecycled(holder: ContactHolder) {
+            super.onViewRecycled(holder)
+            Log.d("APP", "Recycled holder at position ${holder.bindingAdapterPosition}")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,9 +103,17 @@ class MeusJogosFragment : Fragment() {
     ): View {
         binding = FragmentMeusJogosBinding.inflate(inflater, container, false)
         bindEvents()
+        reset()
         return binding.root
     }
     private fun bindEvents() {
+        viewModel.clickedItemId.observe(viewLifecycleOwner){
+            if(viewModel.clickedItemId.value!=null){
+                findNavController().navigate(MeusJogosFragmentDirections.onClickItemMeusJogos(it), navOptions {
+                    popUpTo(R.id.meusJogosFragment)
+                })
+            }
+        }
         binding.bottomNavigationJogos.setOnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.menu_explorar -> findNavController().navigate(R.id.action_meusJogosFragment_to_explorarFragment)
@@ -31,6 +123,26 @@ class MeusJogosFragment : Fragment() {
         }
         binding.fabHistoric.setOnClickListener {
             findNavController().navigate(R.id.action_meusJogosFragment_to_historicoFragment)
+        }
+    }
+
+    private fun reset(){
+        viewModel.getMeusJogos().observe(viewLifecycleOwner) { status ->
+            when(status) {
+                is MainViewModel.Status.Success -> {
+                    (status.result as MainViewModel.Result.GetJogos).value.let{
+                        if(it.isEmpty()){
+                            binding.meusJogosMatchNotFoundTextView.visibility = View.VISIBLE
+                        } else{
+                            binding.recyclerviewMeusJogos.apply {
+                                Log.d("ENTREI","ENTROU NO ADAPTER ${it}")
+                                adapter = ContactAdapter(it)
+                            }
+                        }
+                    }
+                }
+                else -> {}
+            }
         }
     }
 
